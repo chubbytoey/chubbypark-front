556a7a7f595b8f6220246ac4a5d5394c4628663c
chubbytoey <toey.sasikrang@gmail.com>
2020-10-09 00:41:21 +0700

    add GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Suspense } from 'react'
+import { createGlobalStyle } from 'styled-components'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import routes from './config/routes'
 
+const GlobalStyle = createGlobalStyle`
+html,
+body,
+#root {
+  height: 100vh;
+  width: 100vw;
+  font-size: 20px;
+  font-family: "Roboto", sans-serif;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+`
+
 function App () {
   return (
-    <Router>
-      <Suspense>
-        <Switch>
-          {Object.keys(routes).map(routeKey => (
-            <Route key={routeKey} {...routes[routeKey]} />
-          ))}
-        </Switch>
-      </Suspense>
-    </Router>
+    <>
+    <GlobalStyle/>
+      <Router>
+        <Suspense fallback="...loading">
+          <Switch>
+            {Object.keys(routes).map(routeKey => (
+              <Route key={routeKey} {...routes[routeKey]} />
+            ))}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   )
 }
 
