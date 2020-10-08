ccff2e9233c47d63d5d22675ee126bafb5c23773
chubbytoey <toey.sasikrang@gmail.com>
2020-10-09 00:42:04 +0700

    edit navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Navbar = styled.div`
+  background-color:red;
+`
+
+function Navbar() {
+  return (
+    <div className={style.navbar}>yahoooooo</div>
+  )
+}
+export default Navbar
