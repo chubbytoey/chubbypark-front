9ffb61e9c5f38763812ff94393a91d1b0ac6912f
Supphawit-J <ideajeng2543@gmail.com>
2020-10-10 23:51:20 +0700

    add faq page

---
+++ src/pages/Faq.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FaqContent from '../components/FaqContent'
+import Footer from '../components/Footer'
+
+function Faq () {
+  return (
+    <>
+      <FaqContent />
+      <Footer />
+    </>
+  )
+}
+export default Faq
