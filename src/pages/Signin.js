3b944017f804f84fc1993f7da2c51f5bb66e308e
chubbytoey <toey.sasikrang@gmail.com>
2020-10-11 23:34:00 +0700

    edit Signin page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,24 +1,89 @@
 import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styled from 'styled-components'
+import PasswordIcon from '../assets/password-icon.png'
+import UsernameIcon from '../assets/username-icon.png'
+import { Icon } from '../components/IconLayout'
 
 const Content = styled.div`
-  height: 70vh;
-  display:flex;
-  justify-content:center;
-  align-items:center;
+  height: 67vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   /* background:#fff; */
 `
 const SigninBlock = styled.div`
-  width: 30rem;
-  height: 15rem;
-  border-radius:10px;
-  box-shadow: 2px 1px 20px #DBDBDB;
+  width: 28rem;
+  height: 17rem;
+  border-radius: 10px;
+  box-shadow: 2px 1px 20px #dbdbdb;
   background-color: #fff;
-  display:flex;
 `
-const SigninBlockTitle = styled.h1``
+const SigninBlockLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+`
+
+const SigninBlockTitle = styled.h1`
+  font-weight: bold;
+  padding: 1rem 0;
+  font-size: 2rem;
+  text-transform: uppercase;
+`
+
+const SigninInputBlock = styled.div`
+  border: #aaa solid 1px;
+  border-radius: 5px;
+  margin: 0.4rem 0;
+  padding: 0.5rem 1rem;
+  width: 17rem;
+  display: flex;
+`
+
+const SigninInput = styled.input`
+  outline: none;
+  border: none;
+  font-size: 0.8rem;
+  flex: 1;
+
+  &::placeholder {
+    color: #aaa;
+  }
+`
+
+const ForgetPasswordBlock = styled.div`
+  width: 19rem;
+  display: flex;
+  justify-content: flex-end;
+`
+
+const ForgetPassword = styled.h3`
+  color: #aaa;
+  font-size: 0.5rem;
+  text-decoration: underline;
+  text-transform: lowercase;
+`
+
+const SigninButton = styled.button`
+  border: none;
+  outline: none;
+  width: 19rem;
+  font-size: 0.8rem;
+  color: #fff;
+  background-color: #d04394;
+  padding: 0.6rem 1rem;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 1.2rem 0;
+
+  &:hover {
+    background-color: #e04fa2;
+  }
+`
 
 function Signin () {
   return (
@@ -26,9 +91,24 @@ function Signin () {
       <Navbar />
       <Content>
         <SigninBlock>
-          <SigninBlockTitle>
-            SIGN IN
-          </SigninBlockTitle>
+          <SigninBlockLayout>
+            <SigninBlockTitle>sign in</SigninBlockTitle>
+
+            <SigninInputBlock>
+              <SigninInput placeholder='username' />
+              <Icon src={UsernameIcon} />
+            </SigninInputBlock>
+            <SigninInputBlock>
+              <SigninInput placeholder='password' />
+              <Icon src={PasswordIcon} />
+            </SigninInputBlock>
+
+            <ForgetPasswordBlock>
+              <ForgetPassword>Forget Password</ForgetPassword>
+            </ForgetPasswordBlock>
+
+            <SigninButton>LOG IN</SigninButton>
+          </SigninBlockLayout>
         </SigninBlock>
       </Content>
       <Footer />
