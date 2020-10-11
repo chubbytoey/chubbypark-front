8009d1df934ac599c773d5ec2ee2bcac3d57d275
Supphawit-J <ideajeng2543@gmail.com>
2020-10-11 18:39:34 +0700

    edit faq page

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FaqContent from '../components/FaqContent'
 import Footer from '../components/Footer'
+import Navbar from '../components/navbar'
 
 function Faq () {
   return (
     <>
+      <Navbar />
       <FaqContent />
       <Footer />
     </>
