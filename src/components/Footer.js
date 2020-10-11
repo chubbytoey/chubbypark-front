7c0b7f53c7e826efe15989f046da77f11aad98e5
chubbytoey <toey.sasikrang@gmail.com>
2020-10-11 23:33:00 +0700

    add icon-social in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Icon } from '../components/IconLayout'
+import FacebookIcon from '../assets/facebook-icon.png'
+import InstagramIcon from '../assets/instagram-icon.png'
+import TelephoneIcon from '../assets/telephone-icon.png'
 
 function Footer () {
   const FooterContainer = styled.div`
     display: flex;
     background-color: #D04394;
-    height: 35vh;
+    height: 30vh;
     width: 100%;
     justify-content: space-between;
   `
@@ -14,7 +18,7 @@ function Footer () {
     justify-content: flex-end;
     align-items: flex-start;
     flex: 1;
-    padding: 1rem;
+    padding:2rem 1rem;
   `
   const ContentItem = styled.div`
     color: white;
@@ -28,12 +32,15 @@ function Footer () {
     color: white;
     text-transform: uppercase;
     padding-bottom: 1rem;
+    font-weight:bold;
   `
 
   const ItemList = styled.div`
     color: white;
     font-size: 0.7rem;
     margin: 0.3rem 0;
+    font-weight:lighter;
+    display:flex;
   `
 
   const FooterCopyright = styled.div`
@@ -60,9 +67,9 @@ function Footer () {
       <FooterContent>
         <ContentItem>
           <ItemTitle>Contact</ItemTitle>
-          <ItemList>CHUBBYPARK OFFICIAL</ItemList>
-          <ItemList>CHUBBYPARK OFFICIAL</ItemList>
-          <ItemList>099-xxx-xxxx</ItemList>
+          <ItemList><Icon src={FacebookIcon} />CHUBBYPARK OFFICIAL</ItemList>
+          <ItemList><Icon src={InstagramIcon}/>CHUBBYPARK OFFICIAL</ItemList>
+          <ItemList><Icon src={TelephoneIcon}/>099-xxx-xxxx</ItemList>
         </ContentItem>
       </FooterContent>
     </FooterContainer>
