d97e2f34745accad283303bf1e99f7b2877ba21e
chubbytoey <toey.sasikrang@gmail.com>
2020-10-11 23:33:16 +0700

    add iconLayout component

---
+++ src/components/IconLayout.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Icon = styled.div`
+  background-image: url(${props => props.src});
+  width: 1.02rem;
+  height: 0.91rem;
+  background-size: cover;
+`
