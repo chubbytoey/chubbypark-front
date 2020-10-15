2db423f530a46d9b07fb479a209d8fcf9590f4c4
chubbytoey <toey.sasikrang@gmail.com>
2020-10-16 00:34:47 +0700

    add link to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,35 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 import userIcon from '../assets/user-icon.png'
 
 const NavbarContainer = styled.div`
   border: none;
   outline: none;
-  color: #DD4A9E;
+  color: #dd4a9e;
   display: flex;
   flex-direction: column;
   height: 20vh;
-  box-shadow: 2px 1px 7px #DBDBDB;
-  position:relative;
-  z-index:1000;
+  box-shadow: 2px 1px 7px #dbdbdb;
+  position: relative;
+  z-index: 1000;
 `
 const NavbarUserIcon = styled.div`
   background-image: url(${props => props.src});
-  background-size:cover;
-  margin:0 0.5rem;
-  width:1.02rem;
-  height:.91rem;
+  background-size: cover;
+  margin: 0 0.5rem;
+  width: 1.02rem;
+  height: 0.91rem;
 `
 
 const NavbarSignin = styled.div`
   flex: 2;
   display: flex;
   justify-content: flex-end;
-  align-items:center;
+  align-items: center;
   padding: 0 2rem;
   font-size: 0.7rem;
-  font-weight:bold;
-  text-transform:uppercase;
+  font-weight: bold;
+  text-transform: uppercase;
 `
 const NavbarTitle = styled.div`
   flex: 3;
@@ -49,11 +50,11 @@ const NavbarMenu = styled.div`
     list-style-type: none;
   }
   li {
-    font-size:0.9rem;
-    letter-spacing:0.05rem;
+    font-size: 0.9rem;
+    letter-spacing: 0.05rem;
     display: inline;
     padding: 0 1.5rem;
-    text-transform:uppercase;
+    text-transform: uppercase;
   }
 `
 
@@ -67,10 +68,10 @@ function Navbar () {
       <NavbarTitle>CHUBBYPARK</NavbarTitle>
       <NavbarMenu>
         <ul>
-          <li>Home</li>
-          <li>Reservation</li>
-          <li>Promotion</li>
-          <li>FAQ</li>
+          <li><Link to='/'>home</Link></li>
+          <li><Link to='/'>reservation</Link></li>
+          <li><Link to='/'>Promotion</Link></li>
+          <li><Link to='/faq'>faq</Link></li>
         </ul>
       </NavbarMenu>
     </NavbarContainer>
