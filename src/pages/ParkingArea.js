23b178b8362ff16b020d45572a00c874c30bc4b0
chubbytoey <toey.sasikrang@gmail.com>
2020-10-22 23:40:59 +0700

    add check-login mockup ver.

--- src/pages/ParkingArea.js
+++ src/pages/ParkingArea.js
@@ -4,6 +4,8 @@ import Footer from '../components/Footer'
 import ParkingContent from '../components/ParkingContent'
 
 function ParkingArea () {
+  const tokenTest = window.localStorage.getItem('storeToken')
+  tokenTest !== null ? console.log('login') : console.log('not login') // forChecklogin
   return (
     <>
       <Navbar />
