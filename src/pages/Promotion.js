049df0f713384eab079195a9a9ed48ed1de81ee4
chubbytoey <toey.sasikrang@gmail.com>
2020-10-26 03:44:11 +0700

    move fetchFunction to util

--- src/pages/Promotion.js
+++ src/pages/Promotion.js
@@ -1,40 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import ActionContext from '../contexts/ActionContext'
 import styled from 'styled-components'
-/* global fetch */
-async function fetchCustomer (id) {
-  const response = await fetch(`http://127.0.0.1:3333/api/v1/Customers/${id}`)
-
-  return response.json()
-}
-
-function useCustomer (customerId) {
-  // SET STATE
-  const [customer, setCustomer] = useState({})
-  const [isLoading, setLoading] = useState(false)
-  const [error, setError] = useState(undefined)
-
-  //  USE LOCATION DATA
-  async function getCustomer () {
-    try {
-      setLoading(true)
-      const customerResponse = await fetchCustomer(customerId)
-      setCustomer(...customerResponse.data)
-    } catch (err) {
-      setError(err)
-    }
-    setLoading(false)
-  }
-
-  // RETURN DATA
-  return [{ customer, isLoading, error }, { getCustomer }]
-}
 
 const Text = styled.div`
-  font-size:20px;
+  font-size: 20px;
 `
 
-function Promotion () {
-  const [{ customer, isLoading, error }, { getCustomer }] = useCustomer(17)
+function Promotion() {
+  const { customer, isLoading, error, getCustomer } = useContext(ActionContext)
+  // const [{ customer, isLoading, error }, { getCustomer }] = useCustomer()
+  // console.log(customer)
   useEffect(() => {
     getCustomer()
   }, [])
