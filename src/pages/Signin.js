ab403be999b56c0177c0e11f5754250ca6cddcfe
chubbytoey <toey.sasikrang@gmail.com>
2020-10-26 07:24:21 +0700

    move components to authLayout-component

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -5,6 +5,18 @@ import styled from 'styled-components'
 import PasswordIcon from '../assets/password-icon.png'
 import UsernameIcon from '../assets/username-icon.png'
 import { Icon } from '../components/IconLayout'
+import {
+  Content,
+  SignInWrapper,
+  SideButton,
+  SideText,
+  SigninBlock,
+  SigninBlockLayout,
+  SigninBlockTitle,
+  SigninInputBlock,
+  SigninInput,
+  SigninButton
+} from '../components/AuthLayout'
 
 /* global fetch */
 async function FetchResponse(accountDataform) {
@@ -18,79 +30,7 @@ async function FetchResponse(accountDataform) {
   })
   return accountResponse.json()
 }
-const Content = styled.div`
-  height: 67vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #fff;
-`
-const SignInWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-`
-const SideButton = styled.div`
-  width: 2rem;
-  height: 12vh;
-  background-color: #d04394;
-  position: absolute;
-  margin-top: 2rem;
-  border-radius: 10px 0 0 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  &:hover {
-    background-color: #e04fa2;
-  }
-`
-const SideText = styled.div`
-  color: #fff;
-  padding: 0.2rem;
-  writing-mode: vertical-rl;
-  font-size: 0.7rem;
-  transform: rotate(180deg);
-`
-const SigninBlock = styled.div`
-  margin: 0 2rem;
-  width: 28rem;
-  height: 17rem;
-  border-radius: 10px;
-  box-shadow: 2px 1px 20px #dbdbdb;
-  background-color: #fff;
-`
-const SigninBlockLayout = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem;
-`
-const SigninBlockTitle = styled.h1`
-  font-weight: bold;
-  padding: 1rem 0;
-  font-size: 2rem;
-  text-transform: uppercase;
-`
-const SigninInputBlock = styled.div`
-  border: #aaa solid 1px;
-  border-radius: 5px;
-  margin: 0.4rem 0;
-  padding: 0.5rem 1rem;
-  width: 17rem;
-  display: flex;
-`
-const SigninInput = styled.input`
-  outline: none;
-  border: none;
-  font-size: 0.8rem;
-  flex: 1;
 
-  &::placeholder {
-    color: #aaa;
-  }
-`
 const ForgetPasswordBlock = styled.div`
   width: 19rem;
   display: flex;
@@ -101,22 +41,6 @@ const ForgetPassword = styled.h3`
   font-size: 0.5rem;
   text-decoration: underline;
   text-transform: lowercase;
-`
-const SigninButton = styled.button`
-  border: none;
-  outline: none;
-  width: 19rem;
-  font-size: 0.8rem;
-  color: #fff;
-  background-color: #d04394;
-  padding: 0.6rem 1rem;
-  border-radius: 5px;
-  cursor: pointer;
-  margin: 1.2rem 0;
-
-  &:hover {
-    background-color: #e04fa2;
-  }
 `
 
 function Signin() {
@@ -145,7 +69,7 @@ function Signin() {
         'storeToken',
         JSON.stringify(result.access.token)
       )
-      window.localStorage.setItem('username',result.username)
+      window.localStorage.setItem('username', result.username)
       window.location.assign('/')
       // window.open('/')
     }
@@ -161,18 +85,18 @@ function Signin() {
     window.location.assign('/signup')
   }
 
-  useEffect(() => { 
+  useEffect(() => {
     checkLogin()
-  },[window.localStorage.getItem('storeToken')])
+  }, [window.localStorage.getItem('storeToken')])
   return (
     <>
       <Navbar />
-      <Content>
+      <Content height='67'>
         <SignInWrapper>
           <SideButton as="a" onClick={GoSignUp}>
             <SideText>SIGN UP</SideText>
           </SideButton>
-          <SigninBlock>
+          <SigninBlock height='17'>
             <SigninBlockLayout onSubmit={SigninAccount}>
               <SigninBlockTitle>sign in</SigninBlockTitle>
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,114 +6,31 @@ import PasswordIcon from '../assets/password-icon.png'
 import UsernameIcon from '../assets/username-icon.png'
 import Select from 'react-select'
 import { Icon } from '../components/IconLayout'
+import {
+  Content,
+  SignInWrapper,
+  SideButton,
+  SideText,
+  SigninBlock,
+  SigninBlockLayout,
+  SigninBlockTitle,
+  SigninInputBlock,
+  SigninInput,
+  SigninButton
+} from '../components/AuthLayout'
 
 /* global fetch */
 
-const Content = styled.div`
-  height: 85vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* background:#fff; */
-`
-const SignInWrapper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-`
-const SideButton = styled.div`
-  width: 2rem;
-  height: 12vh;
-  background-color: #d04394;
-  position: absolute;
-  margin-top: 2rem;
-  border-radius: 10px 0 0 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  &:hover {
-    background-color: #e04fa2;
-  }
-`
-const SideText = styled.div`
-  color: #fff;
-  padding: 0.2rem;
-  writing-mode: vertical-rl;
-  font-size: 0.7rem;
-  transform: rotate(180deg);
-`
-const SigninBlock = styled.div`
-  margin: 0 2rem;
-  width: 28rem;
-  height: 25rem;
-  border-radius: 10px;
-  box-shadow: 2px 1px 20px #dbdbdb;
-  background-color: #fff;
-`
-const SigninBlockLayout = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem;
-`
-const SigninBlockTitle = styled.h1`
-  font-weight: bold;
-  padding: 1rem 0;
-  font-size: 2rem;
-  text-transform: uppercase;
-`
-const SigninInputBlock = styled.div`
-  border: #aaa solid 1px;
-  border-radius: 5px;
-  margin: 0.4rem 0;
-  padding: 0.5rem 1rem;
-  width: 17rem;
-  display: flex;
-  flex: 1;
-`
 const SigninInputBlockSec = styled.div`
   width: 19rem;
   height: 3rem;
   display: flex;
 `
-const SigninInput = styled.input`
-  outline: none;
-  border: none;
-  font-size: 0.8rem;
-  flex: 1;
-
-  &::placeholder {
-    color: #aaa;
-  }
-`
 const SigninSelect = styled(Select)`
-  /* border: #aaa solid 1px;
-  border-radius: 5px; */
-  /* display: flex; */
-  /* margin: 0.4rem 0; */
-  /* margin-left: 0.3rem; */
   padding-top: 0.4rem;
   padding-left: 0.5rem;
   width: 40rem;
   font-size: 0.7rem;
-`
-const SigninButton = styled.button`
-  border: none;
-  outline: none;
-  width: 19rem;
-  font-size: 0.8rem;
-  color: #fff;
-  background-color: #d04394;
-  padding: 0.6rem 1rem;
-  border-radius: 5px;
-  cursor: pointer;
-  margin: 1.2rem 0;
-
-  &:hover {
-    background-color: #e04fa2;
-  }
 `
 const option = [
   {
@@ -181,7 +98,6 @@ function Signup(event) {
       }
     )
     const result = await accountResponse.json()
-    console.log(result.error)
     if (!result.error) {
       // alert('hey')
       window.location.assign('/signin')
@@ -227,12 +143,12 @@ function Signup(event) {
   return (
     <>
       <Navbar />
-      <Content>
+      <Content height='85'>
         <SignInWrapper>
           <SideButton as="a" onClick={GoSignIn}>
             <SideText>SIGN IN</SideText>
           </SideButton>
-          <SigninBlock>
+          <SigninBlock height='25'>
             <SigninBlockLayout onSubmit={RegisterAccount}>
               <SigninBlockTitle>sign up</SigninBlockTitle>
 
@@ -285,17 +201,6 @@ function Signup(event) {
                     type="date"
                   />
                 </SigninInputBlock>
-                {/* <SigninSelect
-                  required
-                  id="selectValue"
-                  onChange={handleGenderChange}>
-                  <SigninOption value="" selected disabled hidden>
-                    Choose here
-                  </SigninOption>
-                  <SigninOption value="male">male</SigninOption>
-                  <SigninOption value="female">female</SigninOption>
-                  <SigninOption value="lgbt">lgbt</SigninOption>
-                </SigninSelect> */}
                 <SigninSelect
                   id="selectValue"
                   onChange={handleGenderChange}
